5d76f96a448d0de829cdcd947f9a32183a2f987b
Bojan9 <9spasovski@gmail.com>
2026-09-25 13:34:16 +0200

    Fix WASM asset path for updated SDK

--- js/demoApp.js
+++ js/demoApp.js
@@ -765,7 +765,7 @@ createApp({
             let license_key = "just-an-empty-string"; //this is where you input your license key generated on the barkoder portal
 
 
-            this.Barkoder = await BarkoderSDK.initialize(license_key);
+            this.Barkoder = await BarkoderSDK.initialize(license_key, { wasmPath: './js/' });
 
             this.cameras = await this.Barkoder.getCameras();
             
